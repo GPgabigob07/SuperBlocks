6547d12def22fcb028edf744934e5fbf78029082
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-02-10 15:48:52 +0300

    Added proxy

---
+++ src/main/java/com/maxfreemen/superblocks/client/ClientProxy.java
@@ -0,0 +1,32 @@
+package com.maxfreemen.superblocks.client;
+
+import com.maxfreemen.superblocks.common.CommonProxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy extends CommonProxy {
+	
+	@Override
+    public void preInit(FMLPreInitializationEvent event) {
+		
+        super.preInit(event);
+        
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+    	
+        super.init(event);
+        
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+    	
+        super.postInit(event);
+        
+    }
+
+}
