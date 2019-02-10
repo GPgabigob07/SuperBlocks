22b69286897dc6ad973ab60f11396b760e5030ec
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-02-10 15:45:53 +0300

    Tests

---
+++ build/sources/main/java/com/maxfreemen/superblocks/SuperBlocks.java
@@ -0,0 +1,8 @@
+package com.maxfreemen.superblocks;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "superblocks", name = "Super Blocks", version = "alfa-0.1", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/MaxFreemen/SuperBlocks/master/versions.json")
+public class SuperBlocks {
+
+}
