221c2acddb0d2563a28dd410cbcbd9308513a2ac
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-03-08 21:50:17 +0300

    6 new blocks added

---
+++ src/main/java/com/maxfreemen/extendedblocks/common/blocks/BlockPlanksBigOakVertical.java
@@ -0,0 +1,23 @@
+package com.maxfreemen.extendedblocks.common.blocks;
+
+import com.maxfreemen.extendedblocks.init.TabsInit;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockPlanksBigOakVertical extends Block {
+	
+	public BlockPlanksBigOakVertical() {
+		
+		super(Material.WOOD);
+		
+		this.setCreativeTab(TabsInit.MEDIEVAL_BLOCKS);
+		this.setHardness(2.0F); //Скорость добычи как у большинства блоков
+		this.setHarvestLevel("hand", 0); //добыча железной киркой
+		this.setRegistryName("planks_big_oak_vertical");
+		this.setResistance(15.0F); //Взрывоустойчивость как у камня
+		this.setUnlocalizedName("extendedblocks.planks_big_oak_vertical");
+		
+	}
+
+}
---
+++ src/main/java/com/maxfreemen/extendedblocks/common/blocks/BlockPlanksJungleVertical.java
@@ -0,0 +1,23 @@
+package com.maxfreemen.extendedblocks.common.blocks;
+
+import com.maxfreemen.extendedblocks.init.TabsInit;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockPlanksJungleVertical extends Block {
+	
+	public BlockPlanksJungleVertical() {
+		
+		super(Material.WOOD);
+		
+		this.setCreativeTab(TabsInit.MEDIEVAL_BLOCKS);
+		this.setHardness(2.0F); //Скорость добычи как у большинства блоков
+		this.setHarvestLevel("hand", 0); //добыча железной киркой
+		this.setRegistryName("planks_jungle_vertical");
+		this.setResistance(15.0F); //Взрывоустойчивость как у камня
+		this.setUnlocalizedName("extendedblocks.planks_jungle_vertical");
+		
+	}
+
+}
