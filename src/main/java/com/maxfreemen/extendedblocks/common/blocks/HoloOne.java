b402d82629f870ef9ea099623e1257df52762402
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-03-28 22:18:47 +0300

    Block test

---
+++ src/main/java/com/maxfreemen/extendedblocks/common/blocks/HoloOne.java
@@ -0,0 +1,22 @@
+package com.maxfreemen.extendedblocks.common.blocks;
+
+import com.maxfreemen.extendedblocks.init.TabsInit;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class HoloOne extends Block {
+	
+	public HoloOne() {
+		
+		super(Material.IRON);
+		
+		this.setCreativeTab(TabsInit.FACTORY_BLOCKS);
+		this.setHardness(2.0F); //Скорость добычи как у большинства блоков
+		this.setHarvestLevel("pickaxe", 2); //добыча железной киркой
+		this.setRegistryName("holo_one");
+		this.setResistance(15.0F); //Взрывоустойчивость как у камня
+		this.setUnlocalizedName("extendedblocks.holo_one");
+		
+	}
+}
