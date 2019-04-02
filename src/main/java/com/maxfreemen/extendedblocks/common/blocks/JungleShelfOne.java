e04a8670330b067bc4ac1fe0f94174251de3537a
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-04-02 21:32:42 +0300

    fix

--- src/main/java/com/maxfreemen/extendedblocks/common/blocks/JungleShelfOne.java
+++ src/main/java/com/maxfreemen/extendedblocks/common/blocks/JungleShelfOne.java
@@ -2,21 +2,9 @@ package com.maxfreemen.extendedblocks.common.blocks;
 
 import com.maxfreemen.extendedblocks.init.TabsInit;
 import net.minecraft.block.BlockBookshelf;
-import net.minecraft.block.BlockHorizontal;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class JungleShelfOne extends BlockBookshelf {
 	
-	public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
 	public JungleShelfOne() {
 
@@ -27,43 +15,9 @@ public class JungleShelfOne extends BlockBookshelf {
 		this.setRegistryName("jungle_shelf_one");
 		this.setResistance(15.0F); //Взрывоустойчивость как у камня
 		this.setUnlocalizedName("extendedblocks.jungle_shelf_one");
-		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+
 		
 	}
 
-	
-	
-	protected BlockStateContainer createBlockState()
-    {
-        return new BlockStateContainer(this, new IProperty[] {FACING});
-    }
-
-    public IBlockState withRotation(IBlockState state, Rotation rot)
-    {
-        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
-    }
-
-    public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
-    {
-        return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
-    }
-
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
-    {
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-    }
-
-    public int getMetaFromState(IBlockState state)
-    {
-        int i = 0;
-        i = i | ((EnumFacing)state.getValue(FACING)).getHorizontalIndex();
-        return i;
-    }
-    
-    public IBlockState getStateFromMeta(int meta)
-    {
-        return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
-    }
-    
     
 }
