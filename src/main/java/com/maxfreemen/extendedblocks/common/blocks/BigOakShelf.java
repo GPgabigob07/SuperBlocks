24d77c3ad0485b0d15a5657cad8454271026ba14
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-04-02 21:02:49 +0300

    fix

--- src/main/java/com/maxfreemen/extendedblocks/common/blocks/BigOakShelf.java
+++ src/main/java/com/maxfreemen/extendedblocks/common/blocks/BigOakShelf.java
@@ -2,14 +2,11 @@ package com.maxfreemen.extendedblocks.common.blocks;
 
 import com.maxfreemen.extendedblocks.init.TabsInit;
 import net.minecraft.block.BlockBookshelf;
-import net.minecraft.block.BlockHorizontal;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.util.EnumFacing;
+
 
 
 public class BigOakShelf extends BlockBookshelf {
 	
-	public static final PropertyDirection FACING = BlockHorizontal.FACING;
 
 	public BigOakShelf() {
 		
@@ -21,7 +18,7 @@ public class BigOakShelf extends BlockBookshelf {
 		this.setRegistryName("big_oak_shelf");
 		this.setResistance(15.0F); //Взрывоустойчивость как у камня
 		this.setUnlocalizedName("extendedblocks.big_oak_shelf");
-		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+	
 		
 	}
     
