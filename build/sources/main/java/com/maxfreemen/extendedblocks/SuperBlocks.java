09b0cf2e2683814b5ac76d92574728e7ca7b4816
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-04-28 20:36:45 +0300

    Tests

--- build/sources/main/java/com/maxfreemen/extendedblocks/SuperBlocks.java
+++ build/sources/main/java/com/maxfreemen/extendedblocks/SuperBlocks.java
@@ -1,16 +1,28 @@
 package com.maxfreemen.extendedblocks;
 
 import com.maxfreemen.extendedblocks.common.CommonProxy;
+import com.maxfreemen.extendedblocks.tileentity.TileEntityConcreteMixer;
+import com.maxfreemen.extendedblocks.util.Reference;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = "extendedblocks", name = "Extended Blocks", version = "alfa-0.4", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/MaxFreemen/SuperBlocks/master/versions.json")
 public class SuperBlocks {
+	
+	 
+    @Instance
+    public static SuperBlocks instance;
+    {
+    	GameRegistry.registerTileEntity(TileEntityConcreteMixer.class, new ResourceLocation(Reference.MOD_ID + ":concrete_mixer"));
+    }
 
 	@SidedProxy(clientSide = "com.maxfreemen.extendedblocks.client.ClientProxy", serverSide = "com.maxfreemen.extendedblocks.common.CommonProxy")
 	public static CommonProxy proxy;
@@ -35,5 +47,7 @@ public class SuperBlocks {
 	    proxy.postInit(event);
 	    
 	}
+	
+
 	
 }
