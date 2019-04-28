1e630e2ac641a38252779f0140ad495fb5293114
MaxFreemen <47484306+MaxFreemen@users.noreply.github.com>
2019-04-28 19:57:36 +0300

    STILL GUI

--- src/main/java/com/maxfreemen/extendedblocks/SuperBlocks.java
+++ src/main/java/com/maxfreemen/extendedblocks/SuperBlocks.java
@@ -1,6 +1,8 @@
 package com.maxfreemen.extendedblocks;
 
 import com.maxfreemen.extendedblocks.common.CommonProxy;
+import com.maxfreemen.extendedblocks.tileentity.TileEntityConcreteMixer;
+
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -9,6 +11,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = "extendedblocks", name = "Extended Blocks", version = "alfa-0.4", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/MaxFreemen/SuperBlocks/master/versions.json")
 public class SuperBlocks {
@@ -16,6 +19,9 @@ public class SuperBlocks {
 	 
     @Instance
     public static SuperBlocks instance;
+    {
+ GameRegistry.registerTileEntity(TileEntityConcreteMixer.class,"extendedblocks:concrete_mixer");
+    }
 
 	@SidedProxy(clientSide = "com.maxfreemen.extendedblocks.client.ClientProxy", serverSide = "com.maxfreemen.extendedblocks.common.CommonProxy")
 	public static CommonProxy proxy;
--- src/main/java/com/maxfreemen/extendedblocks/util/Reference.java
+++ src/main/java/com/maxfreemen/extendedblocks/util/Reference.java
@@ -1,5 +1,11 @@
 package com.maxfreemen.extendedblocks.util;
 
-public class Reference {
+public class Reference  {
+
+    public static final String MOD_ID = "extendedblocks";
+    public static final String NAME = "Extended Blocks";
+    public static final String VERSION = "${version}";
+    public static final String ACCEPTED_VERSIONS = "${mcversion}";
+   
 
 }
